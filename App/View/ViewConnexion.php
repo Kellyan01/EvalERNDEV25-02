bd71571dabd39a458a33d41d50b2dc241001dd54
Kellyan01 <depriester.yoann@neuf.fr>
2025-06-20 15:58:31 +0200

    Feature : update ViewConnexion.php

---
+++ App/View/ViewConnexion.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-wdth, initial-scale=1.0">
+	<link rel="stylesheet" href="./public/assets/styles/main.css">
+	<script src="./public/assets/scripts/main.js" async></script>
+	<title>Accueil</title>
+</head>
+<body>
+	<h1>Se connecter</h1>
+	<form actin="" method="post">
+		<label for="email">Veuillez saisir votre adresse email : </label>
+		<input type="email3 name="email">
+		<label for="password">Veuillez saisir votre mot de passe : </label>
+		<input type="password" name="password">
+		<input type="submit" name="submit" value="connexion">
+	</form>
+</body>
+</html>
